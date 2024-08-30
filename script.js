0ebc835e9c2ca2dd1e13d1a3af6d89d50ac42ddb
MS7RU <88928636+MS7RU@users.noreply.github.com>
2024-08-30 06:26:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function link(n) {
+            const links = [
+                'https://ms7ru.github.io/maze/',
+                'https://ms7ru.github.io/t',
+                'https://ms7ru.github.io/p',
+                'https://drive.google.com/drive/folders/1ZltQTmU7Db10QA9L2z3AP_8waFz54_vr',
+                'https://ms7ru.github.io/book/book',
+                'https://gracious-swirles-589615.netlify.app/',
+                'https://ms7ru.github.io/PcPartPicker/'
+            ];
+            window.location.href = links[n-1]
+        }
