3e97e29eae54fa4581983a3d383aef51496a122d
MS7RU <88928636+MS7RU@users.noreply.github.com>
2024-09-04 06:46:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function link(n) {
                 'https://drive.google.com/drive/folders/1ZltQTmU7Db10QA9L2z3AP_8waFz54_vr',
                 'https://ms7ru.github.io/book/book',
                 'https://gracious-swirles-589615.netlify.app/',
-                'https://ms7ru.github.io/PcPartPicker/'
+                'https://ms7ru.github.io/PcPartPicker/',
+                'https://ms7ru.github.io/clock'
             ];
             window.location.href = links[n-1]
         }
